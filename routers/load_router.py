b421ee5d69b9114439c49d6c2695074421378eaf
nayeon1107 <quf991107@gmail.com>
2023-08-22 10:32:56 +0900

    230822/na KOPIS DB 함수 추가

--- routers/load_router.py
+++ routers/load_router.py
@@ -2,6 +2,7 @@ import os
 from fastapi import APIRouter, Request
 from utils.load_KOPIS import *
 from utils.load_TMDB import *
+import datetime
 
 router = APIRouter()
 
@@ -9,6 +10,11 @@ router = APIRouter()
 async def get_pf_detail_routes():
     PF_ID_LIST = ['PF223258', 'PF223038', 'PF222985', 'PF222956']
     return get_pf_detail(PF_ID_LIST)
+
+@router.get("/kopis/perfomance-to-db")
+async def get_mt20id_routes():
+    ST_DT = datetime.datetime.now()
+    return get_mt20id(ST_DT)
 
 @router.get("/tmdb/discover-movie")
 async def get_tmdb_discoverMovies_routes(nowDate:str):
