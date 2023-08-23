97a956c7e221fd21ecb0ffb2d878278f409013c3
woorek <lkjwj95@gmail.com>
2023-08-23 12:44:26 +0900

    230823/이우재 imdb_venice.py 스크립트 일부 수정 및 예시 데이터 추가

--- src/imdb_venice.py
+++ src/imdb_venice.py
@@ -1,35 +1,37 @@
-import requests, re, json, os
+import requests, json, os, datetime
 from bs4 import BeautifulSoup as bs
-import datetime
 
-save_path = "/Users/woorek/Downloads/tmp_json/"
+def extract_venice(start_year:int, end_year:int):
 
-start_year = 2020
-end_year = int(datetime.datetime.today().strftime("%Y"))
+    start = start_year
+    end_year = end_year
 
+    # request 요청할 때 필요한 header, params정의
+    header = {
+            "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+        }
+    params = {
+            "ref_":"fea_eds_top-1_3"
+        }
 
-header = {
-        "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
-    }
-params = {
-        "ref_":"fea_eds_top-1_3"
-    }
+    # 설정한 기간으로 데이터 수집
+    for i in range(start_year,end_year+1):
 
-# for i in range(2022,end_year+1):
-for i in range(start_year,end_year+1):
-    url = f"https://www.imdb.com/event/ev0000681/{i}/1/"
+        # response로 raw data 받기
+        url = f"https://www.imdb.com/event/ev0000681/{i}/1/"
+        resp = requests.get(url=url, headers=header, params=params).text
+        soup = bs(resp, "html.parser")
+        script_tags = soup.select('#main > div:nth-child(5) > span > script')
 
-    resp = requests.get(url=url, headers=header, params=params).text
-    soup = bs(resp, "html.parser")
+        # 데이터 transform
+        data = str(script_tags[0])
+        tmp_data = (((data.split(">")[1]).split("<")[0]).split("IMDbReactWidgets.NomineesWidget.push(['center-3-react',")[1]).split("]);")[0]
+        final_dict = json.loads(tmp_data)
 
-    script_tags = soup.select('#main > div:nth-child(5) > span > script')
+        # 데이터 save
+        save_path = "/Users/woorek/Downloads/tmp_json/"
+        file_name = f"imdb_venice_{i}.json"
+        with open(os.path.join(save_path,file_name), "w") as json_file:
+            json.dump(final_dict, json_file, indent=4)
 
-    data = str(script_tags[0])
-    tmp_data = (((data.split(">")[1]).split("<")[0]).split("IMDbReactWidgets.NomineesWidget.push(['center-3-react',")[1]).split("]);")[0]
-    final_dict = json.loads(tmp_data)
-
-    file_name = f"venice_{i}.json"
-    with open(os.path.join(save_path,file_name), "w") as json_file:
-        json.dump(final_dict, json_file, indent=4)
-
-print("끝")
+    print("끝")
