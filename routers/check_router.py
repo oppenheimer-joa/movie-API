2a58fd626b05a8ea68223daf951d62303f3d1e8b
woorek <lkjwj95@gmail.com>
2023-08-28 12:10:31 +0900

    230828/woorek check_router.py 박스오피스 라우팅추가 & check_Boxoffice.py 박스오피스 정합성체크 로직 추가

--- routers/check_router.py
+++ routers/check_router.py
@@ -1,6 +1,7 @@
 import os, datetime
 from fastapi import APIRouter, Request
 from utils.check_IMDB import *
+from utils.check_BoxOffice import *
 
 router = APIRouter()
 
@@ -8,3 +9,8 @@ router = APIRouter()
 @router.get("/check/imdb/"):
 async def check_imdb_routes(event:str, year:int):
 	return imdb_file_check(event,year)
+
+# BoxOffice (Kobis) 데이터 정합성 체크: 데이터가 받아진 디렉토리에서 파일 개수 체크
+@router.get("/check/boxoffice/")
+async def check_boxoffice_routes()
+	return BoxOffice_file_check()
--- utils/check_BoxOffice.py
+++ utils/check_BoxOffice.py
@@ -1,6 +1,6 @@
 import os
 
-def BoxOffice_file_check(now_date):
+def BoxOffice_file_check():
 	directory_path = "/api/datas/kobis"
 	file_cnt = sum(1 for file in os.listdir(directory_path) if file.endswith('.json'))
 	if file_cnt == 17:
