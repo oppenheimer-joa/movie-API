f35c85b41fe81d7fe4c8fdf6bf970679703c794d
woorek <lkjwj95@gmail.com>
2023-08-28 15:05:09 +0900

    230828/woorek KOPIS 공연데이터 정합성 체크 로직 추가

--- routers/check_router.py
+++ routers/check_router.py
@@ -2,6 +2,7 @@ import os, datetime
 from fastapi import APIRouter, Request
 from utils.check_IMDB import *
 from utils.check_BoxOffice import *
+from utils.check_KOPIS import *
 
 router = APIRouter()
 
@@ -10,7 +11,17 @@ router = APIRouter()
 async def check_imdb_routes(event:str, year:int):
 	return imdb_file_check(event,year)
 
-# BoxOffice (Kobis) 데이터 정합성 체크: 데이터가 받아진 디렉토리에서 파일 개수 체크
+# BoxOffice (KOIBS) 데이터 정합성 체크: 데이터가 받아진 디렉토리에서 파일 개수 체크
 @router.get("/check/boxoffice/")
 async def check_boxoffice_routes()
 	return BoxOffice_file_check()
+
+# TMDB 데이터 정합성 체크
+
+
+# KOPIS (공연) 데이터 정합성 체크
+@router.get("/check/kopis/")
+async def check_kopis_routes(st_dt:str, db_cnt:int)
+	return kopis_file_check(st_dt, db_cnt)
+
+# Spotify 데이터 정합성 체크
---
+++ utils/check_KOPIS.py
@@ -0,0 +1,15 @@
+import os, re, datetime
+
+def kopis_file_check(st_dt:str, db_cnt:int):
+	directory_path = "./datas/kopis"
+	xml_files_list = []
+
+    for filename in os.listdir(directory_path):
+    	if filename.endswith(".xml") and st_dt in filename:
+        	xml_files_list.append(filename)
+    xml_file_cnt = len(xml_files_list)
+
+    if xml_file_cnt == db_cnt:
+        return f"DB속 개수({db_cnt})와 파일 개수({xml_file_cnt}) 가 같슴둥"
+    else:
+        return "1"
