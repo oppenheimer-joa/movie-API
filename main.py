edb435bea301776eb4be5efce71c1e146de31f6e
woorek <lkjwj95@gmail.com>
2023-08-31 19:18:11 +0900

    230831/woorek blah main.py cleansing router 추가

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from routers.test import router as test
 from routers.load_router import router as load_router
 from routers.check_router import router as check_router
 from routers.blob_router import router as blob_router
+from routers.cleansing_router import router as cleansing_router
 
 def create_app():
     app = FastAPI()
@@ -11,6 +12,7 @@ def create_app():
     app.include_router(load_router)
     app.include_router(check_router)
     app.include_router(blob_router)
+    app.include_router(cleansing_router)
     return app
 
 app = create_app()
