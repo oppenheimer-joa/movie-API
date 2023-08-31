6a4d94dc2e64c7e1981208e063876c2e13851056
blahxblah <id.blahxblah@gmail.com>
2023-08-31 18:34:15 +0900

    230831/blahxblah main.py blob router 추가

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from fastapi import FastAPI
 from routers.test import router as test
 from routers.load_router import router as load_router
 from routers.check_router import router as check_router
+from routers.blob_router import router as blob_router
 
 def create_app():
     app = FastAPI()
     app.include_router(test)
     app.include_router(load_router)
     app.include_router(check_router)
+    app.include_router(blob_router)
     return app
 
 app = create_app()
